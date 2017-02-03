13e0c3526ebdfc368f356e9e1749f24b3f040841
Alexis Duarte <alexismiguelduarte@gmail.com>
2017-02-02 19:23:09 -0800

    Agregue carpeta vendor para que modifiquen el autoload

--- views/site/index.php
+++ views/site/index.php
@@ -7,11 +7,11 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Bienvenido!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Esta pagina fue hecha para como carta de presentacion de Zula Duarti.</p>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Fierro Pariente</a></p>
     </div>
 
     <div class="body-content">
